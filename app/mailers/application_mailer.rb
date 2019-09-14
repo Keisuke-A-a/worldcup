1b64a6d38d79b98732c066056d251e5580d95f8d
Keisuke-A-a <usd1122334@gmail.com>
2019-09-14 14:06:24 +0900

    herokuapp.comに変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@murmuring-scrubland-59242.heroku.com'
+  default from: 'noreply@murmuring-scrubland-59242.herokuapp.com'
   layout 'mailer'
 end
